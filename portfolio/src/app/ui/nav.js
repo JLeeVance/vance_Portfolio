33c0c910f9a6a95ea9417fa5b49cc98fc4d419c4
J Vance <justinleevance94@gmail.com>
2024-05-14 14:49:40 -0400

    initial styling of nav bar

--- portfolio/src/app/ui/nav.js
+++ portfolio/src/app/ui/nav.js
@@ -12,7 +12,7 @@ export default function Nav(){
 
     let navOptions = paths.map((path) => {
         return(
-            <Link
+            <Link class='p-1 mx-1 border-2 rounded-full border-lime-900/50 bg-slate-50 text-slate-500 hover:bg-lime-900/50 hover:text-white hover:underline hover:border-lime-900'
                 key={path.name}
                 href={path.href}>
                 {path.name}
@@ -22,9 +22,9 @@ export default function Nav(){
 
 
     return(
-        <>
+        <div class='flex flex-row bg-lime-900 py-5'>
             {navOptions}    
-        </>
+        </div>
         
     )
 
